84bf47f964718b7ff0fd64e5427e310c73496b9e
b04901142 <31992609+b04901142@users.noreply.github.com>
2017-10-09 16:09:10 +0800

    Update client_main.c

--- client_main.c
+++ client_main.c
@@ -24,17 +24,30 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <wiringPi.h>
 
 #include "gattlib.h"
+
+#define BUTTON_PIN0 0
+#define BUTTON_PIN1 4
 
 typedef enum { READ, WRITE} operation_t;
 operation_t g_operation;
 
 static uuid_t g_uuid;
 long int value_data;
+volatile int eventCounter = 0;
 
 static void usage(char *argv[]) {
-	printf("%s <device_address> <read|write> <uuid> [<hex-value-to-write>]\n", argv[0]);
+	printf("%s <device_address>\n", argv[0]);
+}
+
+void myirq1(gatt_connection_t* connection) {
+	value_data = strtol("0x6162", NULL, 0);
+	ret = gattlib_write_char_by_uuid(connection, &g_uuid, value_data, sizeof(value_data));
+	assert(ret == 0);
 }
 
 int main(int argc, char *argv[]) {
@@ -43,52 +56,35 @@ int main(int argc, char *argv[]) {
 	size_t len;
 	gatt_connection_t* connection;
 
-	if ((argc != 4) && (argc != 5)) {
+	if ((argc != 2)) {
 		usage(argv);
 		return 1;
 	}
-
-	if (strcmp(argv[2], "read") == 0) {
-		g_operation = READ;
-	} else if ((strcmp(argv[2], "write") == 0) && (argc == 5)) {
-		g_operation = WRITE;
-
-		if ((strlen(argv[4]) >= 2) && (argv[4][0] == '0') && (argv[4][0] == 'x')) {
-			value_data = strtol(argv[4], NULL, 0);
-		} else {
-			value_data = strtol(argv[4], NULL, 16);
-		}
-		printf("Value to write: 0x%lx\n", value_data);
-	} else {
+	// add the uuid
+	if (gattlib_string_to_uuid("", strlen("") + 1, &g_uuid) < 0) {
 		usage(argv);
 		return 1;
 	}
-
-	if (gattlib_string_to_uuid(argv[3], strlen(argv[3]) + 1, &g_uuid) < 0) {
-		usage(argv);
-		return 1;
-	}
-
+	//connect
 	connection = gattlib_connect(NULL, argv[1], BDADDR_LE_PUBLIC, BT_SEC_LOW, 0, 0);
 	if (connection == NULL) {
 		fprintf(stderr, "Fail to connect to the bluetooth device.\n");
 		return 1;
 	}
-
-	if (g_operation == READ) {
-		len = sizeof(buffer);
-		ret = gattlib_read_char_by_uuid(connection, &g_uuid, buffer, &len);
-		assert(ret == 0);
-
-		printf("Read UUID completed: ");
-		for (i = 0; i < len; i++)
-			printf("%02x ", buffer[i]);
-		printf("\n");
-	} else {
-		ret = gattlib_write_char_by_uuid(connection, &g_uuid, buffer, sizeof(buffer));
-		assert(ret == 0);
+	//set up wiringPi
+	if (wiringPiSetup () < 0) {
+		fprintf (stderr, "Unable to setup wiringPi: %s\n", strerror (errno));
+		return 1;
 	}
-
+	pinMode(BUTTON_PIN0, INPUT);
+	//待測試,因為原本的按鈕模組就有拉電阻了
+	pullUpDnControl (BUTTON_PIN0, PUD_UP);
+	
+	if ( wiringPiISR (BUTTON_PIN0, INT_EDGE_FALLING, &myirq1) < 0 ) {
+		fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno));
+		return 1;
+	}
 	gattlib_disconnect(connection);
 	return 0;
 }
+
